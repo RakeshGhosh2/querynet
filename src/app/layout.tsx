0ad893097098452d0e11bdc6ab968e3d2b68df6d
RakeshGhosh2 <145191202+RakeshGhosh2@users.noreply.github.com>
2025-08-17 00:26:53 +0530

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,18 @@ import { cn } from "@/lib/utils";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "QueryNet", 
+  title: "QueryNet",
   description: "Welcome to QueryNet",
   icons: {
-    icon: "/querynet/public/images/querynet2.png", 
+    icon: "/querynet/public/images/querynet2.png",
   },
 
 };
@@ -25,17 +28,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      
+
       <body className={cn(inter.className, "dark:bg-black dark:text-white")}>
+
+
         <Header />
-        {children}
+        <CopilotKit publicApiKey={process.env.API_KEY}>
+          {children}
+        </CopilotKit>
         <Toaster
           position="top-center"
           reverseOrder={false}
         />
         <Footer />
+
+
       </body>
     </html>
+
+
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,22 @@ import HeroSectionHeader from "./components/HeroSectionHeader";
 import HeroSection from "./components/HeroSection";
 import LatestQuestions from "./components/LatestQuestions";
 import TopContributers from "./components/TopContributers";
+import { CopilotSidebar } from "@copilotkit/react-ui";
+
 
 export default function Home() {
   return (
 
     <main>
+      
+      <CopilotSidebar
+      defaultOpen={false}
+      instructions={"You are assisting the user as best as you can. Answer in the best way possible given the data you have."}
+      labels={{
+        title: "QueryNet Helper", 
+        initial: "How can I help you today?",
+      }}
+    >
       <HeroSectionHeader />
       <HeroSection />
       <h2 className="text-2xl px-15 font-semibold mb-4">Latest Questions :-</h2>
@@ -20,6 +31,7 @@ export default function Home() {
           <TopContributers />
         </div>
       </div>
+      </CopilotSidebar>
     </main>
   );
 }
