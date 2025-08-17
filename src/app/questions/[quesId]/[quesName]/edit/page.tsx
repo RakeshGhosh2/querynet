8aced3f759c2d5c5e496c181e67c2999b3b24bd4
RakeshGhosh2 <145191202+RakeshGhosh2@users.noreply.github.com>
2025-08-17 18:56:08 +0530

    a

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,10 +1,37 @@
+// import { db, questionCollection } from "@/models/name";
+// import { databases } from "@/models/server/config";
+// import React from "react";
+// import EditQues from "./EditQues";
+
+// const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
+//     const question = await databases.getDocument(db, questionCollection, params.quesId);
+
+//     return <EditQues question={question} />;
+// };
+
+// export default Page;
+
+import React from "react";
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
-import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+// 👇 Force this page to run in Node.js (not Edge)
+export const runtime = "nodejs";
+
+type Props = {
+    params: {
+        quesId: string;
+        quesName: string;
+    };
+};
+
+const Page = async ({ params }: Props) => {
+    const question = await databases.getDocument(
+        db,
+        questionCollection,
+        params.quesId
+    );
 
     return <EditQues question={question} />;
 };
