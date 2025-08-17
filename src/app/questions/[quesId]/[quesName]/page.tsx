580c8e8a788eb86321a8853e5d1768687d6c73a3
RakeshGhosh2 <145191202+RakeshGhosh2@users.noreply.github.com>
2025-08-17 15:16:09 +0530

    add

--- src/app/questions/[quesId]/[quesName]/page.tsx
+++ src/app/questions/[quesId]/[quesName]/page.tsx
@@ -2,8 +2,8 @@ import Answers from "@/components/Answers";
 import Comments from "@/components/Comments";
 import { MarkdownPreview } from "@/components/RTE";
 import VoteButtons from "@/components/VoteButtons";
-import {Particles} from "@/components/magicui/particles";
-import {ShimmerButton} from "@/components/magicui/shimmer-button";
+import { Particles } from "@/components/magicui/particles";
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
 import { avatars } from "@/models/client/config";
 import {
     answerCollection,
@@ -25,6 +25,7 @@ import React from "react";
 import DeleteQuestion from "./DeleteQuestion";
 import EditQuestion from "./EditQuestion";
 import { TracingBeam } from "@/components/ui/tracing-beam";
+import { CopilotSidebar } from "@copilotkit/react-ui";
 
 const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
     const [question, answers, upvotes, downvotes, comments] = await Promise.all([
@@ -121,108 +122,117 @@ const Page = async ({ params }: { params: { quesId: string; quesName: string } }
     ]);
 
     return (
-        <TracingBeam className="container pl-6">
-            <Particles
-                className="fixed inset-0 h-full w-full"
-                quantity={500}
-                ease={100}
-                color="#ffffff"
-                refresh
-            />
-            <div className="relative mx-auto px-4 pb-20 pt-36">
-                <div className="flex">
-                    <div className="w-full">
-                        <h1 className="mb-1 text-3xl font-bold">{question.title}</h1>
-                        <div className="flex gap-4 text-sm">
-                            <span>
-                                Asked {convertDateToRelativeTime(new Date(question.$createdAt))}
-                            </span>
-                            <span>Answer {answers.total}</span>
-                            <span>Votes {upvotes.total + downvotes.total}</span>
-                        </div>
-                    </div>
-                    <Link href="/questions/ask" className="ml-auto inline-block shrink-0">
-                        <ShimmerButton className="shadow-2xl">
-                            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-                                Ask a question
-                            </span>
-                        </ShimmerButton>
-                    </Link>
-                </div>
-                <hr className="my-4 border-white/40" />
-                <div className="flex gap-4">
-                    <div className="flex shrink-0 flex-col items-center gap-4">
-                        <VoteButtons
-                            type="question"
-                            id={question.$id}
-                            className="w-full"
-                            upvotes={upvotes}
-                            downvotes={downvotes}
-                        />
-                        <EditQuestion
-                            questionId={question.$id}
-                            questionTitle={question.title}
-                            authorId={question.authorId}
-                        />
-                        <DeleteQuestion questionId={question.$id} authorId={question.authorId} />
-                    </div>
-                    <div className="w-full overflow-auto">
-                        <MarkdownPreview className="rounded-xl p-4" source={question.content} />
-                        <picture>
-                            <img
-                                src={
-                                    storage.getFilePreview(
-                                        questionAttachmentBucket,
-                                        question.attachmentId
-                                    )//href
-                                }
-                                alt={question.title}
-                                className="mt-3 rounded-lg"
-                            />
-                        </picture>
-                        <div className="mt-3 flex flex-wrap items-center gap-3 text-sm">
-                            {question.tags.map((tag: string) => (
-                                <Link
-                                    key={tag}
-                                    href={`/questions?tag=${tag}`}
-                                    className="inline-block rounded-lg bg-white/10 px-2 py-0.5 duration-200 hover:bg-white/20"
-                                >
-                                    #{tag}
-                                </Link>
-                            ))}
-                        </div>
-                        <div className="mt-4 flex items-center justify-end gap-1">
-                            <picture>
-                                <img
-                                    src={avatars.getInitials(author.name, 36, 36)}//href
-                                    alt={author.name}
-                                    className="rounded-lg"
-                                />
-                            </picture>
-                            <div className="block leading-tight">
-                                <Link
-                                    href={`/users/${author.$id}/${slugify(author.name)}`}
-                                    className="text-orange-500 hover:text-orange-600"
-                                >
-                                    {author.name}
-                                </Link>
-                                <p>
-                                    <strong>{author.prefs.reputation}</strong>
-                                </p>
+        <CopilotSidebar
+            defaultOpen={false}
+            instructions={"You are assisting the user as best as you can. Answer in the best way possible given the data you have."}
+            labels={{
+                title: "QueryNet Helper",
+                initial: "How can I help you today?",
+            }}
+        >
+            <TracingBeam className="container pl-6">
+                <Particles
+                    className="fixed inset-0 h-full w-full"
+                    quantity={500}
+                    ease={100}
+                    color="#ffffff"
+                    refresh
+                />
+                <div className="relative mx-auto px-4 pb-20 pt-36">
+                    <div className="flex">
+                        <div className="w-full">
+                            <h1 className="mb-1 text-3xl font-bold">{question.title}</h1>
+                            <div className="flex gap-4 text-sm">
+                                <span>
+                                    Asked {convertDateToRelativeTime(new Date(question.$createdAt))}
+                                </span>
+                                <span>Answer {answers.total}</span>
+                                <span>Votes {upvotes.total + downvotes.total}</span>
                             </div>
                         </div>
-                        <Comments
-                            comments={comments}
-                            className="mt-4"
-                            type="question"
-                            typeId={question.$id}
-                        />
-                        <hr className="my-4 border-white/40" />
+                        <Link href="/questions/ask" className="ml-auto inline-block shrink-0">
+                            <ShimmerButton className="shadow-2xl">
+                                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                                    Ask a question
+                                </span>
+                            </ShimmerButton>
+                        </Link>
                     </div>
+                    <hr className="my-4 border-white/40" />
+                    <div className="flex gap-4">
+                        <div className="flex shrink-0 flex-col items-center gap-4">
+                            <VoteButtons
+                                type="question"
+                                id={question.$id}
+                                className="w-full"
+                                upvotes={upvotes}
+                                downvotes={downvotes}
+                            />
+                            <EditQuestion
+                                questionId={question.$id}
+                                questionTitle={question.title}
+                                authorId={question.authorId}
+                            />
+                            <DeleteQuestion questionId={question.$id} authorId={question.authorId} />
+                        </div>
+                        <div className="w-full overflow-auto">
+                            <MarkdownPreview className="rounded-xl p-4" source={question.content} />
+                            <picture>
+                                <img
+                                    src={
+                                        storage.getFilePreview(
+                                            questionAttachmentBucket,
+                                            question.attachmentId
+                                        )//href
+                                    }
+                                    alt={question.title}
+                                    className="mt-3 rounded-lg"
+                                />
+                            </picture>
+                            <div className="mt-3 flex flex-wrap items-center gap-3 text-sm">
+                                {question.tags.map((tag: string) => (
+                                    <Link
+                                        key={tag}
+                                        href={`/questions?tag=${tag}`}
+                                        className="inline-block rounded-lg bg-white/10 px-2 py-0.5 duration-200 hover:bg-white/20"
+                                    >
+                                        #{tag}
+                                    </Link>
+                                ))}
+                            </div>
+                            <div className="mt-4 flex items-center justify-end gap-1">
+                                <picture>
+                                    <img
+                                        src={avatars.getInitials(author.name, 36, 36)}//href
+                                        alt={author.name}
+                                        className="rounded-lg"
+                                    />
+                                </picture>
+                                <div className="block leading-tight">
+                                    <Link
+                                        href={`/users/${author.$id}/${slugify(author.name)}`}
+                                        className="text-orange-500 hover:text-orange-600"
+                                    >
+                                        {author.name}
+                                    </Link>
+                                    <p>
+                                        <strong>{author.prefs.reputation}</strong>
+                                    </p>
+                                </div>
+                            </div>
+                            <Comments
+                                comments={comments}
+                                className="mt-4"
+                                type="question"
+                                typeId={question.$id}
+                            />
+                            <hr className="my-4 border-white/40" />
+                        </div>
+                    </div>
+                    <Answers answers={answers} questionId={question.$id} />
                 </div>
-                <Answers answers={answers} questionId={question.$id} />
-            </div>
-        </TracingBeam>
+            </TracingBeam>
+        </CopilotSidebar>
     );
 };
 
--- src/app/questions/ask/page.tsx
+++ src/app/questions/ask/page.tsx
@@ -1,12 +1,22 @@
 'use client';
 
 import QuestionForm from '@/components/QuestionForm';
+import { CopilotSidebar } from '@copilotkit/react-ui';
 
 
 export default function Home() {
     return (
         <div>
-            <QuestionForm />
+            <CopilotSidebar
+                defaultOpen={false}
+                instructions={"You are assisting the user as best as you can. Answer in the best way possible given the data you have."}
+                labels={{
+                    title: "QueryNet Helper",
+                    initial: "How can I help you today?",
+                }}
+            >
+                <QuestionForm />
+            </CopilotSidebar>
         </div>
     );
 
