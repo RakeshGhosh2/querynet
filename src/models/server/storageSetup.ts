695def4de96cc538cb99ceb21f9edc24605e0810
RakeshGhosh2 <145191202+RakeshGhosh2@users.noreply.github.com>
2025-08-18 21:59:04 +0530

    as

--- src/models/server/storageSetup.ts
+++ src/models/server/storageSetup.ts
@@ -1,9 +1,9 @@
 export const runtime = "nodejs";
 
-import { Permission } from "node-appwrite";
+import { Permission } from "appwrite";
 import { questionAttachmentBucket } from "../name";
 import { storage } from "./config";
-import { AppwriteException } from "node-appwrite";
+import { AppwriteException } from "appwrite";
 
 export default async function getOrCreateStorage() {
     try {
@@ -36,42 +36,3 @@ export default async function getOrCreateStorage() {
         }
     }
 }
-
-
-// import { Permission, Role } from "node-appwrite";
-// import { questionAttachmentBucket } from "../name";
-// import { storage } from "./config";
-
-// export default async function getOrCreateStorage() {
-//     try {
-//         // Try fetching the existing bucket
-//         await storage.getBucket(questionAttachmentBucket);
-//         console.log("Storage Connected");
-//     } catch (error: any) {
-//         if (error.code === 404) {
-//             try {
-//                 await storage.createBucket(
-//                     questionAttachmentBucket,       // bucketId
-//                     questionAttachmentBucket,       // name
-//                     [
-//                         Permission.read(Role.any()),      // Public read
-//                         Permission.read(Role.users()),    // Users can read
-//                         Permission.update(Role.users()),  // Users can update
-//                         Permission.delete(Role.users()),  // Users can delete
-//                     ],
-//                     false,                            // fileSecurity (disable)
-//                     "auto",                           // enabled
-//                     "file",                           // type (can be 'file' or 'image')
-//                     ["jpg", "png", "gif", "jpeg", "webp", "heic"] // allowed file extensions
-//                 );
-
-//                 console.log("Storage Created");
-//                 console.log("Storage Connected");
-//             } catch (error) {
-//                 console.error("❌ Error creating storage:", error);
-//             }
-//         } else {
-//             console.error("❌ Error accessing bucket:", error);
-//         }
-//     }
-// }
