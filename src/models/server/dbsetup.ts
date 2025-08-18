f67bd9c71eaf116002857e12fefb1d82a6c45f50
RakeshGhosh2 <145191202+RakeshGhosh2@users.noreply.github.com>
2025-08-18 23:09:59 +0530

    Update dbsetup.ts

--- src/models/server/dbsetup.ts
+++ src/models/server/dbsetup.ts
@@ -1,37 +1,3 @@
-// import {db} from "../name"
-// import creatQuestionCollection from "./question.collection"
-// import createAnswerCollection from "./answer.collection"
-// import createCommentCollection from "./comment.collection"
-// import createVoteCollection from "./vote.collection"
-// import { databases } from "./config"
-
-
-// export default async function getOrCreateDB() {
-//     try {
-//         await databases.get (db)
-//         console.log("Database connected")
-//         } catch (error) {
-//             try {
-//                 await databases.create (db,db)
-//                 console.log("Database created")
-
-//                 await Promise.all([
-//                     creatQuestionCollection(),
-//                     createAnswerCollection(),
-//                     createCommentCollection(),
-//                     createVoteCollection(),
-
-//                 ])
-//                 console.log("Collections created")
-//                 console.log("Database connected")
-//             } catch (error) {
-//                 console.log(" Error creating database", error)
-//             }
-
-//         }
-//         return databases
-
-// }
 
 export const runtime = "nodejs";
 
